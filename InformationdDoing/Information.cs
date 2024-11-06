badd730bea2c73fe010038b35fdedb09059ae41c
Дария <@LAPTOP-4L6TPS96>
2024-11-06 21:18:15 +0200

    Add project files.

---
+++ InformationdDoing/Information.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformationdDoing
+{
+    public class Information
+    {
+        private string city;
+        private string name;
+        private int age;
+        private string gender;
+        private string address;
+
+        public string City 
+        { 
+            get { return city; } 
+            set { city = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (age < 18 && age < 60)
+                {
+                    Console.WriteLine("Wrong age!");
+                }   
+            } 
+        }
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public void Output()
+        {
+            Console.WriteLine($"{this.city}, {this.name}, {this.age}, {this.gender}, {this.address}");
+        }
+
+        public Information(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public Information(string city, string name, int age, string gender, string address)
+        {
+            this.city = city;
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.address = address;
+        }
+
+    }
+}
---
+++ InformationdDoing/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using InformationdDoing;
+using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("Enter people: ");
+Information person = new Information("Stefan", 12);
+person.Output();
+var n = int.Parse(Console.ReadLine());
+string searchName = Console.ReadLine();
+for (int i = 0; i < n; i++)
+{
+   var danni = Console.ReadLine().Split();
+    Information person2 = new Information(danni[0], int.Parse(danni[1]));
+    List<Information> list = new List<Information>();
+    list.Add(person2);
+    Console.WriteLine(list.Min(x => x.Age));
+    list.Where(x => x.Name == searchName).ToList().ForEach(x => x.Output());
+    list.OrderBy(x => x.Name).ToList().ForEach(x => x.Output());
+  
+}
+
+try
+{
+
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (ArgumentOutOfRangeException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
